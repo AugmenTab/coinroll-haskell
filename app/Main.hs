d2189f899657847a4eeaf4a963b57dcb5ce2208a
AugmenTab <tbaum92@live.com>
2021-07-19 22:54:23 -0600

    Added hello world code, experiencing type error.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
-import Lib
+{-# LANGUAGE OverloadedStrings #-}
+import Network.HTTP.Types (status200)
+import Network.Wai
+import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
-main = someFunc
+main = run 8000 $ \_req send ->
+  send $ responseBuilder
+    status200
+    [("Content-Type", "text/plain; charset=utf-8")]
+    "Hello, World!"
